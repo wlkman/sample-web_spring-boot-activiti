0a9a37f3403307c0c094e34f1c9a8a938f3d9a31
wlkman <cuixinbin@yeah.net>
2020-01-07 14:39:17 +0800

    cross domain

--- src/main/java/com/cuixb/activiti/config/CorsFilter.java
+++ src/main/java/com/cuixb/activiti/config/CorsFilter.java
@@ -16,11 +16,11 @@ import org.springframework.context.annotation.Configuration;
 public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Origin","http://localhost:6008");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         chain.doFilter(req, res);
     }
 
